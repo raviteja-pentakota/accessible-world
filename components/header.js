d77f5a130db110f7b3caeb28e1bdadc2646dc10c
raviteja-pentakota <raviteja26j@gmail.com>
2024-08-11 19:19:48 +0530

    commit_message

--- components/header.js
+++ components/header.js
@@ -3,51 +3,51 @@ function addStyles() {
   body {
     font-family: Arial, sans-serif;
   }
-  
+
   header {
     background-color: #333;
     color: #fff;
     padding: 10px 0;
     text-align: center;
   }
-  
+
   header #branding {
     display: flex;
     align-items: center;
     justify-content: center;
   }
-  
+
   header #branding #logo {
     width: 50px;
     height: 50px;
     margin-right: 10px;
   }
-  
+
   header h1 {
     font-size: 24px;
     margin: 0;
   }
-  
+
   header nav ul {
     list-style: none;
     padding: 0;
   }
-  
+
   header nav ul li {
     display: inline;
     margin: 0 10px;
   }
-  
+
   header nav ul li a {
     color: #fff;
     text-decoration: none;
     font-weight: bold;
   }
-  
+
   header nav ul li a:hover {
     text-decoration: underline;
   }
-  
+
   .container {
     display: flex;
     flex-direction: column;
@@ -69,8 +69,7 @@ function getHeader() {
       <div id="branding">
         <img src="Accessible_world_logo.jpg" height="83" alt="Accessible World Tutorials Logo">
         <h1>Welcome to Accessible World Tutorials</h1>
-<p>Learning should be inclusive and accessible for all. At Accessible World Tutorials, we offer free courses, paid courses, and personalized instruction to meet your needs. Follow the links below to explore and start your learning journey with us!</p>
-
+        <p>Learning should be inclusive and accessible for all. At Accessible World Tutorials, we offer free courses, paid courses, and personalized instruction to meet your needs. Follow the links below to explore and start your learning journey with us!</p>
       </div>
       <hr role="separator">
       <nav>
@@ -89,7 +88,7 @@ function getHeader() {
         <h2>Latest Update:</h2>
 
         <br>
-<p>Our next Accessibility Testing Batch starts on October 3rd!<br>For full details, visit the Paid Courses section.</p>
+        <p>Our next Accessibility Testing Batch starts on October 3rd!<br>For full details, visit the Paid Courses section.</p>
       </article>
       <hr role="separator">
     </div>
@@ -99,9 +98,8 @@ function getHeader() {
   return headerContent;
 }
 
-
 document.addEventListener("DOMContentLoaded", function () {
   addStyles();
   const headerContent = document.getElementById("headerContent");
-  headerContent.innerHTML = getHeader();
+  headerContent.insertAdjacentHTML('beforeend', getHeader());
 });
